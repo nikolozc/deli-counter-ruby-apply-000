e33c57a2120bcf603b0f261400f03251d93131c3
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-09-24 18:02:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
 # Write your code here.
+<<<<<<< HEAD
 
+=======
+>>>>>>> 137c0d2448e26f29edea6f23ab01033b38d27ad3
 katz_deli = []
 
 def line(katz_deli)
   if(katz_deli.empty?)
     puts "The line is currently empty."
   else
+<<<<<<< HEAD
     text = "The line is currently:"
     counter = 0
     while counter < katz_deli.length do
@@ -13,6 +17,14 @@ def line(katz_deli)
       counter+=1
     end
     puts text
+=======
+    print "The line is currently: "
+    counter = 0
+    while(counter < katz_deli.size) do
+      print "#{counter+1}. #{katz_deli[counter]} "
+      counter+=1
+    end
+>>>>>>> 137c0d2448e26f29edea6f23ab01033b38d27ad3
   end
 end
 
